a18b30fd6272ad52a281336a1c40078e40f7a414
Darkness <Boostgamedota2@yandex.ru>
2026-03-18 09:01:00 +0300

    weather

---
+++ Assets/!MyProject/Scripts/Weather/WeatherCycle.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class WeatherCycle : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
